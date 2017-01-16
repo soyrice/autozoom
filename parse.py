6f071133c9cc48e61912e7d0dac862c2fe46cdfb
Cyrus Maden <cyrus_maden@brown.edu>
2017-01-15 21:37:35 -0800

    Create parse.py

---
+++ parse.py
@@ -0,0 +1,22 @@
+parse(df) :
+    latDict, lonDict = dict(), dict()
+
+    # identify extreme most latitude and longitude coordinate pairs in each state
+    # save coordinate pair of most extreme points for autozoom
+    for count in range(0,len(usStates['features'])) :
+        if geo['key'] in [code for code in statePctChange['code']] :
+            for coords in geo['key']['coordinates'][0] :
+                # collect lat, lon data in either geoJSON format
+                # lat, lon data will either be in list (coords) or a list of lists (pairs) 
+                try :
+                    latDict[coords[1]] = coords
+                    lonDict[coords[0]] = coords
+                except :
+                    for pair in coords :
+                        latDict[pair[1]] = pair
+                        lonDict[pair[0]] = pair
+
+    bounds = [list(reversed(l)) for l in [latDict[max([key for key in latDict.keys()])], latDict[min([key for key in latDict.keys()])],
+        lonDict[max([key for key in lonDict.keys()])], lonDict[min([key for key in lonDict.keys()])]]]
+
+    return bounds
